327fb293d3f2efb3475e4bdafbca148d090b7065
2000JCH <rkwkal59@naver.com>
2025-11-24 15:24:15 +0900

    jwt로 로그인 성공시 list페이지 이동, 실패시 알림창 띄우기 및 코드 개선

--- src/main/java/com/apple/shop/member/JwtFilter.java
+++ src/main/java/com/apple/shop/member/JwtFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -29,6 +28,9 @@ public class JwtFilter extends OncePerRequestFilter {
             return; // 리턴값이 없으면 리턴 밑에 있는 코드는 실행 X
         }
 
+        //쿠키가 있을때만 로그를 찍어줘 (즉 로그인 성공시에만 출력)
+        System.out.println("[JwtFilter] 요청 URI: " + request.getRequestURI() + ", 메서드: " + request.getMethod());
+
         // 1. 이름이 jwt인 쿠키 찾기
         String jwtCookie = "";
         for (int i = 0; i < cookies.length; i++) {
@@ -36,7 +38,6 @@ public class JwtFilter extends OncePerRequestFilter {
                 jwtCookie = cookies[i].getValue();
             }
         }
-        System.out.println(jwtCookie);
 
         // 2. 유효기간, 위조여부 등 확인해보기
         Claims claim;  // 유저의 정보가 들어있음
@@ -52,23 +53,24 @@ public class JwtFilter extends OncePerRequestFilter {
         String[] arr = claim.get("authorities").toString().split(","); //권한을 리스트로 만들기
         List<SimpleGrantedAuthority> authorities = Arrays.stream(arr).map(a -> new SimpleGrantedAuthority(a)).toList();
 
-        var customUser = new CustomUser(
+        CustomUser customUser = new CustomUser(
                 claim.get("username").toString(),
                 "none",
                 authorities
 
         ); //username, password, authority 필수적으로 넣어야함.
 
-
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                claim.get("username").toString(), ""    //유저네임, 패스워드은 필수, 여기있는게 auth.getPrincipal()에 추가됨
+                customUser,
+                null,
+                authorities
+                //claim.get("username").toString(), ""    //유저네임(사용자에 대한 상세정보를 담은 객체일 수도 있음), 패스워드은 필수, 여기있는게 auth.getPrincipal()에 추가됨
                 //많은 정보를 넣고 싶으면 CustomUser()을 하나 만들어서 넣는다.
         );
         authToken.setDetails(new WebAuthenticationDetailsSource()   //실제 auth 변수와 동일하게 만들고 싶을 때 사용
                 .buildDetails(request)
         );
         SecurityContextHolder.getContext().setAuthentication(authToken);
-
 
         // request -> 유저의 여러 정보가 들어있음 ip, 기기종류 등
         filterChain.doFilter(request, response);
--- src/main/java/com/apple/shop/member/MemberController.java
+++ src/main/java/com/apple/shop/member/MemberController.java
@@ -1,9 +1,11 @@
 package com.apple.shop.member;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -58,26 +61,34 @@ public class MemberController {
 
     @PostMapping("/login/jwt")
     @ResponseBody
-    public String loginJWT(@RequestBody Map<String, String> data, HttpServletResponse response) {
-        //로그인 시켜주세요, auth 만들기
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                data.get("username"), data.get("password")
-        );
-        Authentication auth = authenticationManagerBuilder.getObject().authenticate(authToken); //아이디 + 비번 -> id, password를 DB내용과 비교해서 로그인 시켜줌 (수동 로그인 해준거)
-        SecurityContextHolder.getContext().setAuthentication(auth); // 로그인됨이라고 시스템에 등록
+    public ResponseEntity<String> loginJWT(@RequestBody Map<String, String> data, HttpServletResponse response) {
+        try{
+            //로그인 시켜주세요, auth 만들기
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    data.get("username"), data.get("password")
+            );
+            Authentication auth = authenticationManagerBuilder.getObject().authenticate(authToken); //아이디 + 비번 -> id, password를 DB내용과 비교해서 로그인 시켜줌 (수동 로그인 해준거) // MyUserDetailsService 호출
+            SecurityContextHolder.getContext().setAuthentication(auth); // 로그인됨이라고 시스템에 등록
 
-        //JWT 만들어서 보내주기
-        String jwt = JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());
-        //System.out.println(jwt);
+            //JWT 만들어서 보내주기
+            String jwt = JwtUtil.createToken(SecurityContextHolder.getContext().getAuthentication());
+            //System.out.println(jwt);
 
-        //쿠키에 저장해주세요~
-        var cookie = new Cookie("jwt", jwt);
-        cookie.setMaxAge(10); //JWT 유효기간이랑 비슷하게 또는 길게 하면 됨
-        cookie.setHttpOnly(true);   // 해킹범이 쿠키를 자바스크립트로 조작하기 어려워짐
-        cookie.setPath("/");    //쿠키가 전송될 URL
-        response.addCookie(cookie);
+            //쿠키에 저장해주세요~
+            Cookie cookie = new Cookie("jwt", jwt);
+            cookie.setMaxAge(10); //JWT 유효기간이랑 비슷하게 또는 길게 하면 됨
+            cookie.setHttpOnly(true);   // 해킹범이 쿠키를 자바스크립트로 조작하기 어려워짐
+            cookie.setPath("/");    //쿠키가 전송될 URL
+            response.addCookie(cookie);
 
-        return jwt;
+            System.out.println("로그인 성공 JWT: " + jwt);
+
+            return ResponseEntity.ok(jwt);
+
+        } catch (Exception e){
+            System.out.println("로그인 실패");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 실패");
+        }
     }
     @GetMapping("/my-page/jwt")
     @ResponseBody
